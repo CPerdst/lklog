7d2c96f7c9b508c86a9793e1d9513768e8264fa2
maziyang <maziyang@eaglecloud.com>
2025-01-08 16:26:32 +0800

    fix：修复CRT_BUILD_TYPE相关以及DLL_API相关宏定义

--- src/include/tools.h
+++ src/include/tools.h
@@ -2,17 +2,19 @@
 #define LKLOG_TOOLS_H
 
 #if defined(_WIN32) || defined(_WIN64)  // Windows ??
-#ifdef EXPORTING_DLL
-#define DLL_API __declspec(dllexport)
-#else
-#define DLL_API __declspec(dllimport)
-#endif
+    #if defined(STATIC_BUILD)
+        #define DLL_API
+    #elif defined(EXPORTING_DLL)
+        #define DLL_API __declspec(dllexport)
+    #else
+        #define DLL_API __declspec(dllimport)
+    #endif
 #else  // ?? Windows ?? (Linux, macOS ??)
-#ifdef EXPORTING_DLL
-#define DLL_API __attribute__ ((visibility ("default")))
-#else
-#define DLL_API
-#endif
+    #ifdef EXPORTING_DLL
+        #define DLL_API __attribute__ ((visibility ("default")))
+    #else
+        #define DLL_API
+    #endif
 #endif
 
 #endif // LKLOG_TOOLS_H
